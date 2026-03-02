3fbdd6eb525342f9e7a6aba38c7ca682e7fc1fda
Youhorng <2023221kean@aupp.edu.kh>
2026-03-02 15:10:27 +0700

    feat: add apis

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    name: str
+    email: str
+
+# GET
 @app.get("/")
 def root():
     return {"message": "Hello from FastAPI!"}
@@ -13,3 +19,18 @@ def health():
 @app.get("/users")
 def get_users():
     return {"users": ["Alice", "Bob", "Charlie"]}
+
+# POST
+@app.post("/users")
+def create_user(user: User):
+    return {"message": "User created", "user": user}
+
+# PUT
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User):
+    return {"message": f"User {user_id} updated", "user": user}
+
+# DELETE
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    return {"message": f"User {user_id} deleted"}
